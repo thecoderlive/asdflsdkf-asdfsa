c7823b119aaeed61910867f61027b7cde4fec72b
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-07 00:18:12 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8bcddfe5-6dc0-47a5-aad9-4172ce130115",
+    "home_feed": [
+        {
+            "id": "e15b8f1a-26ab-4801-a2c3-a8c578339da2",
+            "artist_name": "close",
+            "home_carousel": [
+                {
+                    "id": "42df6f96-1578-4d29-9307-8a2914669b6b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "fed80ec0-1865-4d82-8d5c-054610dc53fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "4cd26400-ba4f-42ea-b9e1-b7c5fba038c7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "48c0ca92-bed2-4c48-97e2-696a747b3e18",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "01833161-1b5b-44b2-81f1-c6629037f500",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                }
+            ],
+            "art_caption": "follow",
+            "art_description": "Miss rock push business. Rest open stock. Radio next listen arm wall unit ahead war. Cup tree public read what identify pay team."
+        },
+        {
+            "id": "af404243-e8c2-4a3d-941c-5a7055df33c5",
+            "artist_name": "service",
+            "home_carousel": [
+                {
+                    "id": "da3e28cc-530b-4f57-994a-ff2ffa19be1f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "8a51e4e4-4b24-4ff0-9d2b-18f6ec692ef5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                },
+                {
+                    "id": "283937d8-d2ce-44e6-aa5b-c0c8cff92e22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "3eb84e8e-f48f-4f64-bec0-b8a8d0603f30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "906e3a43-ff89-4e6a-9b03-06f66eb7c5f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                }
+            ],
+            "art_caption": "trade",
+            "art_description": "Skill option once one. Order year if goal of guy save."
+        },
+        {
+            "id": "99f526d2-4e30-4155-b558-3ca7b9118901",
+            "artist_name": "glass",
+            "home_carousel": [
+                {
+                    "id": "b6911d47-1d94-47f2-9478-a0b51b3bfba0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/189.png"
+                },
+                {
+                    "id": "1156697b-bf4e-4c97-9e44-4a3585acbf2d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "88694fb2-c5dc-4a44-b9a0-8ccf03b478a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "018f0d9e-eabe-48e2-8a88-e7158f0a3f41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                },
+                {
+                    "id": "2d8d3d05-dbad-4c94-9933-78e6df96938f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                }
+            ],
+            "art_caption": "long",
+            "art_description": "Power theory research factor change their hold. Plant future put."
+        },
+        {
+            "id": "12091be8-d88d-4665-8a6f-531515a8b947",
+            "artist_name": "weight",
+            "home_carousel": [
+                {
+                    "id": "602fcdf9-ac53-4a99-81ea-41314c6b1037",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "3d24dc08-ce06-4ede-b210-7f869a05ea5a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "9e909938-92d1-4889-91e6-aefcad1e55ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "364e92ec-70f5-4fb6-8059-e455bbc9b874",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "b088e59c-9857-421c-9917-98748fb4c278",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                }
+            ],
+            "art_caption": "society",
+            "art_description": "Authority himself yet wear west. Wonder enter break number father drop set between. Country nothing color."
+        },
+        {
+            "id": "d41f28ac-c78f-4771-9aa2-fbe918e2e0e9",
+            "artist_name": "note",
+            "home_carousel": [
+                {
+                    "id": "7e59e9d6-496a-4fa4-8278-76f2eb5a7ab0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "d329b6a4-8ef5-4340-9be6-eb5f78caa1e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "22a5f06b-7495-4c91-b498-ef3e9d8952e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "fca92cf9-3dda-494d-b816-e09967ae0b80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "277dadf9-c851-4ae5-bfe6-9dc791c07ff6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                }
+            ],
+            "art_caption": "key",
+            "art_description": "So lead cell bank. Make now him. Rule right medical leave enough save threat factor."
+        }
+    ]
+}
